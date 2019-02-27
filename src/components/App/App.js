6952e2439ed3bef300420dc90186211c998762cf
Vasilis Georgoudis <masakaftw@hotmail.com>
2019-02-27 10:03:31 -0500

    Basic components + stracture

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App
+        <Header/>
+        <Main/>
       </div>
     );
   }
