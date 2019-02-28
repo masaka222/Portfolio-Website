3f52a6e1b4d8d928f2c0dd7a495c65e3786d74ee
Vasilis Georgoudis <masakaftw@hotmail.com>
2019-02-28 00:11:49 -0500

    Sidebar component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './Main.css';
+import Sidebar from '../Sidebar/Sidebar';
 
 class Main extends Component {
   render() {
     return (
       <div className="main">
-        
+        <Sidebar/>
       </div>
     );
   }
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className="sidebar">
+        <div className="sidebar__wrapper"></div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
