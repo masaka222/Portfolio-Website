59f744724f6b462ff3c4cc7a235ccab8e54e56b9
Vasilis Georgoudis <masakaftw@hotmail.com>
2019-03-01 15:57:58 -0500

    Sidebar text positioning

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,7 +5,24 @@ class Sidebar extends Component {
   render() {
     return (
       <div className="sidebar">
-        <div className="sidebar__wrapper"></div>
+        <div className="sidebar__wrapper">
+            <ul className="sidebar__wrapper-list">
+                <li className="sidebar__wrapper-list-item">00</li>
+                <li className="sidebar__wrapper-list-item">01</li>
+                <li className="sidebar__wrapper-list-item">02</li>
+                <li className="sidebar__wrapper-list-item">03</li>
+                <li className="sidebar__wrapper-list-item">04</li>
+            </ul>
+        </div>
+        <div className="sidebar__navigation">
+            <ul className="sidebar__navigation-list">
+                <li className="sidebar__navigation-list-item">Vasilios Georgoudis</li>
+                <li className="sidebar__navigation-list-item">What I Do</li>
+                <li className="sidebar__navigation-list-item">How I Do It</li>
+                <li className="sidebar__navigation-list-item">More Skills</li>
+                <li className="sidebar__navigation-list-item">Contact Me</li>
+            </ul>
+        </div> 
       </div>
     );
   }
