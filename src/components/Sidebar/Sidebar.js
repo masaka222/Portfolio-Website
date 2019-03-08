847b86f92f04e2f2f5a6371413236212cf28b2d8
Vasilis Georgoudis <masakaftw@hotmail.com>
2019-03-07 23:42:02 -0500

    Rewrite of Sidebar component with improvements

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,22 +5,42 @@ class Sidebar extends Component {
   render() {
     return (
       <div className="sidebar">
-        <div className="sidebar__wrapper">
-            <ul className="sidebar__wrapper-list">
-                <li className="sidebar__wrapper-list-item">00</li>
-                <li className="sidebar__wrapper-list-item">01</li>
-                <li className="sidebar__wrapper-list-item">02</li>
-                <li className="sidebar__wrapper-list-item">03</li>
-                <li className="sidebar__wrapper-list-item">04</li>
-            </ul>
-        </div>
         <div className="sidebar__navigation">
             <ul className="sidebar__navigation-list">
-                <li className="sidebar__navigation-list-item">Vasilios Georgoudis</li>
-                <li className="sidebar__navigation-list-item">What I Do</li>
-                <li className="sidebar__navigation-list-item">How I Do It</li>
-                <li className="sidebar__navigation-list-item">More Skills</li>
-                <li className="sidebar__navigation-list-item">Contact Me</li>
+                <li className="sidebar__navigation-list-item">
+                  <a href="#">
+                    <span className="list-black-bar"></span>
+                    <h4 className="list-number">00</h4>
+                    <h4 className="list-text">Vasilios Georgoudis</h4>
+                  </a>
+                </li>
+                <li className="sidebar__navigation-list-item">
+                  <a href="#">
+                    <span className="list-black-bar"></span>
+                    <h4 className="list-number">01</h4>
+                    <h4 className="list-text">What I do</h4>
+                  </a>
+                </li>
+                <li className="sidebar__navigation-list-item">
+                  <a href="#">
+                    <span className="list-black-bar"></span>
+                    <h4 className="list-number">02</h4>
+                    <h4 className="list-text">How I do it</h4>
+                  </a>
+                </li>
+                <li className="sidebar__navigation-list-item"><a href="#">
+                    <span className="list-black-bar"></span>
+                    <h4 className="list-number">03</h4>
+                    <h4 className="list-text">More Skills</h4>
+                  </a>
+                </li>
+                <li className="sidebar__navigation-list-item">
+                  <a href="#">
+                    <span className="list-black-bar"></span>
+                    <h4 className="list-number">04</h4>
+                    <h4 className="list-text">Contact Me</h4>
+                  </a>
+                </li>
             </ul>
         </div> 
       </div>
