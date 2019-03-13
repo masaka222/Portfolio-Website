e34c33b8d752b8444ad97039166cc522ff68a7e3
Vasilis Georgoudis <masakaftw@hotmail.com>
2019-03-13 19:50:54 -0400

    Header start

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import './Header.css';
+import linkedin from './linkedin-logo.png';
+import github from './github-logo.png';
 
 class Header extends Component {
   render() {
     return (
       <header className="header">
-        
+        <div className="logo">
+          <div className="logo__text">
+            <p>Vasilis</p>
+            <p>Georgoudis</p>
+          </div>
+        </div>
+        <div className="center_navigation">
+          <a> About </a>
+          <a> Work </a>
+        </div>
+        <div className="social">
+          <img src={linkedin} className="social__linkedin"></img>
+          <img src={github} className="social__github"></img>
+        </div>
       </header>
     );
   }
