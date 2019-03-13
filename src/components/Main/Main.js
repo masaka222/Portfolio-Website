bc9dd263b860d51728fe972fd4fea4d8951f23e1
Vasilis Georgoudis <masakaftw@hotmail.com>
2019-03-12 21:40:13 -0400

    Main picture centered

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './Main.css';
 import Sidebar from '../Sidebar/Sidebar';
+import profilePicture from './me-drawing.png';
 
 class Main extends Component {
   render() {
     return (
       <div className="main">
         <Sidebar/>
+        <img src={profilePicture} className="main__picture"></img>
       </div>
     );
   }
