dc8d823a47d765bd86509ea93ccbd684a89b93d4
Vasilis Georgoudis <masakaftw@hotmail.com>
2024-08-23 00:08:59 +0300

    Updates

---
+++ precache-manifest.d772cccd94da61578cb7c6a7cee549fe.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0f21e9ed5ea07946724",
+    "url": "/Portfolio-Website/static/css/main.21ff2f7d.chunk.css"
+  },
+  {
+    "revision": "a0f21e9ed5ea07946724",
+    "url": "/Portfolio-Website/static/js/main.beb0e921.chunk.js"
+  },
+  {
+    "revision": "b02ecd064c38077ee4dc",
+    "url": "/Portfolio-Website/static/js/runtime~main.e524495c.js"
+  },
+  {
+    "revision": "0acd112d843597eb47ff",
+    "url": "/Portfolio-Website/static/js/2.f4fdd83f.chunk.js"
+  },
+  {
+    "revision": "379a1f2f2baa0df00df5be5b992d6544",
+    "url": "/Portfolio-Website/static/media/github-logo.379a1f2f.png"
+  },
+  {
+    "revision": "1e9faec399250e775d7dccb4ec82d0d9",
+    "url": "/Portfolio-Website/static/media/vasilis4@0,33x-compressor.1e9faec3.png"
+  },
+  {
+    "revision": "92543a84e2cdd0badcbb7165ad54c3d7",
+    "url": "/Portfolio-Website/static/media/aueb@0,33x-compressor.92543a84.png"
+  },
+  {
+    "revision": "a264ff6df0e92198c55932b065ec49ca",
+    "url": "/Portfolio-Website/static/media/logos-compressor.a264ff6d.png"
+  },
+  {
+    "revision": "bf63a7ca1d520eff50a9b03596e87ec5",
+    "url": "/Portfolio-Website/static/media/laptop-compressor.bf63a7ca.png"
+  },
+  {
+    "revision": "8648b49cd0a0e511e7dea2b62c93c436",
+    "url": "/Portfolio-Website/static/media/mom@0,1x-compressor.8648b49c.png"
+  },
+  {
+    "revision": "c3d7a4b89e7e05fdc69e2beaa6766a34",
+    "url": "/Portfolio-Website/static/media/portfolio-gif.c3d7a4b8.gif"
+  },
+  {
+    "revision": "ae7d8e98bf55d33e4674a5020647a5d4",
+    "url": "/Portfolio-Website/static/media/bullseye@0,1x-compressor.ae7d8e98.png"
+  },
+  {
+    "revision": "8f9e87e33538a72ad1e816af404a980c",
+    "url": "/Portfolio-Website/static/media/mountains@0,33x-compressor.8f9e87e3.png"
+  },
+  {
+    "revision": "0cd603b19d651880aeb611d3e7b27556",
+    "url": "/Portfolio-Website/static/media/camera2@0,25x-compressor.0cd603b1.png"
+  },
+  {
+    "revision": "e83775a3ecebb7dccd738aa4ccab412f",
+    "url": "/Portfolio-Website/static/media/area-white@0,75x-compressor.e83775a3.png"
+  },
+  {
+    "revision": "c1e26d73be371773cd5b3983304d4e00",
+    "url": "/Portfolio-Website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b6ce532a8da6743fbbc1db8f3ef8492.js"
+  "/Portfolio-Website/precache-manifest.d772cccd94da61578cb7c6a7cee549fe.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Portfolio-Website/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
