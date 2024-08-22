57be1b50c717da06c4098e184b75d4780f743861
Vasilis Georgoudis <masakaftw@hotmail.com>
2024-08-22 21:38:32 +0300

    Updates

---
+++ precache-manifest.0b6ce532a8da6743fbbc1db8f3ef8492.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e05bc4f281d8123d7140",
+    "url": "/static/css/main.21ff2f7d.chunk.css"
+  },
+  {
+    "revision": "e05bc4f281d8123d7140",
+    "url": "/static/js/main.e406b367.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0acd112d843597eb47ff",
+    "url": "/static/js/2.f4fdd83f.chunk.js"
+  },
+  {
+    "revision": "379a1f2f2baa0df00df5be5b992d6544",
+    "url": "/static/media/github-logo.379a1f2f.png"
+  },
+  {
+    "revision": "1e9faec399250e775d7dccb4ec82d0d9",
+    "url": "/static/media/vasilis4@0,33x-compressor.1e9faec3.png"
+  },
+  {
+    "revision": "92543a84e2cdd0badcbb7165ad54c3d7",
+    "url": "/static/media/aueb@0,33x-compressor.92543a84.png"
+  },
+  {
+    "revision": "a264ff6df0e92198c55932b065ec49ca",
+    "url": "/static/media/logos-compressor.a264ff6d.png"
+  },
+  {
+    "revision": "bf63a7ca1d520eff50a9b03596e87ec5",
+    "url": "/static/media/laptop-compressor.bf63a7ca.png"
+  },
+  {
+    "revision": "8648b49cd0a0e511e7dea2b62c93c436",
+    "url": "/static/media/mom@0,1x-compressor.8648b49c.png"
+  },
+  {
+    "revision": "c3d7a4b89e7e05fdc69e2beaa6766a34",
+    "url": "/static/media/portfolio-gif.c3d7a4b8.gif"
+  },
+  {
+    "revision": "ae7d8e98bf55d33e4674a5020647a5d4",
+    "url": "/static/media/bullseye@0,1x-compressor.ae7d8e98.png"
+  },
+  {
+    "revision": "8f9e87e33538a72ad1e816af404a980c",
+    "url": "/static/media/mountains@0,33x-compressor.8f9e87e3.png"
+  },
+  {
+    "revision": "0cd603b19d651880aeb611d3e7b27556",
+    "url": "/static/media/camera2@0,25x-compressor.0cd603b1.png"
+  },
+  {
+    "revision": "e83775a3ecebb7dccd738aa4ccab412f",
+    "url": "/static/media/area-white@0,75x-compressor.e83775a3.png"
+  },
+  {
+    "revision": "75cd48e1c2f98ba915365f4177e50888",
+    "url": "/index.html"
+  }
+];
